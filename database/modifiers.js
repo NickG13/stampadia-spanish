9b8b3837fb88e5f5d1e3e8dc56e8b32fbf943dce
NickG13 <eonelgamer@gmail.com>
2023-12-20 13:44:42 +0100

    Update modifiers.js

--- database/modifiers.js
+++ database/modifiers.js
@@ -6,48 +6,48 @@ function loadModifiers() {
 			enemy:{
 				id:"[CODEX-Modifiers] Enemy - Crippled: Enemies moves -1.",
 				type:"good",
-				roomDescription:"{ifEnemyPerformAction:MOVE}{then}It drags forward crippled, {actionEffect:-1}"
+				roomDescription:"{ifEnemyPerformAction:MOVE}{then}Se arrastra, paralizado, {actionEffect:-1}"
 			},
 			hero:{
 				id:"[CODEX-Modifiers] Hero - Paralysis: Hero moves -1.",
 				type:"bad",
-				roomDescription:"{ifHeroPerformAction:MOVE}{then}Your legs feel so heavy, {actionEffect:-1}"
+				roomDescription:"{ifHeroPerformAction:MOVE}{then}Piernas paralizadas, {actionEffect:-1}"
 			},
 		},
 		scared:{
 			enemy:{
 				id:"[CODEX-Modifiers] Enemy - Fear: Enemies -1 a die of choice.",
 				type:"good",
-				roomDescription:"{ifAfterEnemyRollInFight}{then}It trembles with fear, {modifyDice:1,-1}"
+				roomDescription:"{ifAfterEnemyRollInFight}{then}Tiembla de miedo, {modifyDice:1,-1}"
 			},
 			hero:{
 				id:"[CODEX-Modifiers] Hero - Fear: Hero -1 a die of choice.",
 				type:"bad",
-				roomDescription:"{ifAfterHeroRollInFight}{then}Fear makes you flinch, {modifyDice:1,-1}"
+				roomDescription:"{ifAfterHeroRollInFight}{then}Retrocedes con miedo, {modifyDice:1,-1}"
 			}
 		},
 		blind:{
 			enemy:{
 				id:"[CODEX-Modifiers] Enemy - Blind: Enemies plays just 1 die.",
 				type:"good",
-				roomDescription:"{ifAfterEnemyRollInFight}{then}It can't see you, {activateOnly:1}"
+				roomDescription:"{ifAfterEnemyRollInFight}{then}No te puede ver, {activateOnly:1}"
 			},
 			hero:{
 				id:"[CODEX-Modifiers] Hero - Blind: Hero plays just 1 die.",
 				type:"bad",
-				roomDescription:"{ifAfterHeroRollInFight}{then}You see nothing, {activateOnly:1}"
+				roomDescription:"{ifAfterHeroRollInFight}{then}No ves nada, {activateOnly:1}"
 			},
 		},
 		stunned:{
 			enemy:{
 				id:"[CODEX-Modifiers] Enemy - Stunned: Hero rerolls his 1s.",
 				type:"good",
-				roomDescription:"{ifHeroRolls:1}{then}The enemy looks stunned, {reroll:1}"
+				roomDescription:"{ifHeroRolls:1}{then}Enemigo aturdido, {reroll:1}"
 			},
 			hero:{
 				id:"[CODEX-Modifiers] Enemy - Haste: Enemies rerolls his 1s.",
 				type:"bad",
-				roomDescription:"{ifEnemyRolls:1}{then}It's moving so quickly, {reroll:1}"
+				roomDescription:"{ifEnemyRolls:1}{then}Se mueve muy rápido, {reroll:1}"
 			},
 		},
 
@@ -56,28 +56,28 @@ function loadModifiers() {
 			room:{
 				id:"[CODEX-Modifiers] Terrain - Mud: Everybody moves -1.",
 				type:"balanced",
-				roomDescription:"{ifPerformAction:MOVE}{then}The muddy floor slows you down, {actionEffect:-1}"
+				roomDescription:"{ifPerformAction:MOVE}{then}El suelo lodoso te frena, {actionEffect:-1}"
 			}
 		},
 		tallGrass:{
 			room:{
 				id:"[CODEX-Modifiers] Terrain - Tall grass: Everybody -1 a die of choice.",
 				type:"balanced",
-				roomDescription:"{ifAfterRollInFight}{then}The tall grass hinders, {modifyDice:1,-1}"
+				roomDescription:"{ifAfterRollInFight}{then}La maleza te entorpece, {modifyDice:1,-1}"
 			}
 		},
 		darkness:{
 			room:{
 				id:"[CODEX-Modifiers] Terrain - Darkness: Everybody plays just 1 die.",
 				type:"balanced",
-				roomDescription:"{ifAfterRollInFight}{then}It's too dark here, {activateOnly:1}"
+				roomDescription:"{ifAfterRollInFight}{then}Está muy oscuro, {activateOnly:1}"
 			}
 		},
 		arena:{
 			room:{
 				id:"[CODEX-Modifiers] Terrain - Arena: Everybody rerolls his 1s.",
 				type:"balanced",
-				roomDescription:"{ifRolls:1}{then}It's a fight to the death, {reroll:1}"
+				roomDescription:"{ifRolls:1}{then}Es una lucha a muerte, {reroll:1}"
 			}
 		}
 	};
