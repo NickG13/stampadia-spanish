4ab89e06c71d9afd33d7ae8712077c640d70d036
NickG13 <eonelgamer@gmail.com>
2023-12-20 13:33:46 +0100

    Update keywords.js

--- database/keywords.js
+++ database/keywords.js
@@ -6,66 +6,66 @@ function loadKeywords() {
 			{
 				// [CODEX-Keywords] Keyword - Computer: Part of the The End Of Stampadia storyline.
 				id:"computer",
-				label:"Computer"
+				label:"Ordenador"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Irony: Part of the The End Of Stampadia storyline.
 				id:"electricity",
-				label:"Electricity"
+				label:"Electricidad"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Spaceship: Part of the The End Of Stampadia storyline.
 				id:"spaceship",
-				label:"Spaceship"
+				label:"Astronave"
 			},
 			{
 				// [CODEX-Keywords] Keyword - City: Part of the The End Of Stampadia storyline.
 				id:"city",
-				label:"City"
+				label:"Ciudad"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Ink: Part of the The End Of Stampadia storyline.
 				id:"ink",
-				label:"Ink"
+				label:"Tinta"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Press: Part of the The End Of Stampadia storyline.
 				id:"press",
-				label:"Press"
+				label:"Prensa"
 			},
 			{
 				// [CODEX-Keywords] Keyword - End: Part of the The End Of Stampadia storyline.
 				id:"end",
-				label:"End"
+				label:"Final"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Luck: It may bring you good luck during your adventures...
 				id:"luck",
-				label:"Luck"
+				label:"Suerte"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Unluck: It may bring you bad luck during your adventures...
 				id:"unluck",
-				label:"Unluck"
+				label:"Gafe"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Time: It can help you to break a time loop...
 				id:"time",
-				label:"Time"
+				label:"Tiempo"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Break: It can help you to break a time loop...
 				id:"break",
-				label:"Break"
+				label:"Roto"
 			},
 			{
 				// [CODEX-Keywords] Keyword - Loop: It can help you to break a time loop...
 				id:"loop",
-				label:"Loop"
+				label:"Bucle"
 			},
 			{ id:"golem-1", label:"Golem 1" },{ id:"golem-2", label:"Golem 2" },{ id:"golem-3", label:"Golem 3" },
-			{ id:"manticore-1", label:"Manticore 1" },{ id:"manticore-2", label:"Manticore 2" },{ id:"manticore-3", label:"Manticore 3" },
-			{ id:"hellhound-1", label:"Hellhound 1" },{ id:"hellhound-2", label:"Hellhound 2" },{ id:"hellhound-3", label:"Hellhound 3" },
+			{ id:"manticore-1", label:"Mantícora 1" },{ id:"manticore-2", label:"Mantícora 2" },{ id:"manticore-3", label:"Mantícora 3" },
+			{ id:"hellhound-1", label:"Cerbero 1" },{ id:"hellhound-2", label:"Cerbero 2" },{ id:"hellhound-3", label:"Cerbero 3" },
 		],
 		sets:[
 			{
@@ -76,12 +76,12 @@ function loadKeywords() {
 			{
 				// [CODEX-Keywords] Keyword set - Manticore: Beastcrafters collectable cards from 1 to 3.
 				id:"manticoreSet",
-				label:"Manticore"
+				label:"Mantícora"
 			},
 			{
 				// [CODEX-Keywords] Keyword set - Hellhound: Beastcrafters collectable cards from 1 to 3.
 				id:"hellhoundSet",
-				label:"Hellhound"
+				label:"Cerbero"
 			}
 		]
 	};
